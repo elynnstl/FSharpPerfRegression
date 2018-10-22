785c511cbfe16ee609ca6e978f8ad35d45c0957f
Eric Lynn <elynn@deltapointsolutions.com>
2018-10-22 14:30:11 -0500

    Initial commit.

---
+++ FSharp423/FSharpPerfTests.fs
@@ -0,0 +1,44 @@
+﻿namespace FSharpTests
+
+open Microsoft.FSharp.Control
+open Xunit
+
+module FSharpPerfTests =
+
+    [<assembly: CollectionBehavior(DisableTestParallelization = true, MaxParallelThreads = 1)>]
+    do
+        ()
+
+    [<Fact>]
+    let postAndReply() =
+        let agent = 
+            MailboxProcessor.Start(
+                    (fun mbox -> 
+                        async {
+                            while true do
+                                let! msg = mbox.Receive()
+                                match msg with
+                                | (message, chnl:AsyncReplyChannel<unit>) -> 
+                                    chnl.Reply(())
+                        }
+                    ))
+
+        for i in 1 .. 10000 do
+            agent.PostAndReply(fun chnl -> ("hello", chnl))
+
+    [<Fact>]
+    let postAndReplyWithCancellation() =
+        let agent = 
+            MailboxProcessor.Start(
+                    (fun mbox -> 
+                        async {
+                            while true do
+                                let! msg = mbox.Receive()
+                                match msg with
+                                | (message, chnl:AsyncReplyChannel<unit>) -> 
+                                    chnl.Reply(())
+                        }
+                    ), Async.DefaultCancellationToken)
+
+        for i in 1 .. 10000 do
+            agent.PostAndReply(fun chnl -> ("hello", chnl))
